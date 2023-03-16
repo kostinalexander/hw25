77f71f49ee401ef2cb64fd59685982096d077fc7
krut_ <egorrak3221@mail.ru>
2023-03-16 21:38:41 +0300

    Проверьте, пожалуйста

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
     public static void main(String[] args) {
+        Car[] cars = {
+                new Car("car1", 4),
+                new Car("car2", 4),
+        };
+
+        Truck[] trucks = {
+                new Truck("track1", 6),
+                new Truck("truck2", 8),
+        };
+
+        Bicycle[] bicycle = {
+                new Bicycle("bicyvle1", 2),
+                new Bicycle("bicycle2", 2),
+        };
+        ServiceStation serviceStation = new ServiceStation();
+
+        Transport[] transport = new Transport[cars.length + trucks.length + bicycle.length];
+        for (int i = 0; i < cars.length; i++) {
+            transport[i] = cars[i];
+        }
+        for (int i = 0; i < trucks.length; i++) {
+            transport[i + cars.length] = trucks[i];
+        }
+        for (int i = 0; i < bicycle.length; i++) {
+            transport[i + cars.length + trucks.length] = bicycle[i];
+        }
+
+        printReport(serviceStation, cars, bicycle, trucks);
+
+
+    }
+
+    private static void printReport(ServiceStation serviceStation, Car[] cars, Bicycle[] bicycle, Truck[] trucks) {
+        serviceStation.check(cars[0], bicycle[0], trucks[0]);
+        serviceStation.check(cars[1], bicycle[1], trucks[1]);
+        serviceStation.check(null, bicycle[0], null);
+        serviceStation.check(null, bicycle[1], null);
+        serviceStation.check(null, null, trucks[0]);
+        serviceStation.check(null, null, trucks[1]);
 
     }
 }
